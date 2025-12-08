551c2e5bef836ca9976af1e01e8e52d6d62dde28
karimisharon038 <karimisharon038@gmail.com>
2025-12-08 21:25:27 +0300

    Add files via upload

---
+++ api.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const API_BASE = process.env.REACT_APP_API_BASE || 'http://localhost:5000/api';
+
+const instance = axios.create({ baseURL: API_BASE });
+
+export function setToken(token) {
+  if (token) instance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  else delete instance.defaults.headers.common['Authorization'];
+}
+
+export default instance;
