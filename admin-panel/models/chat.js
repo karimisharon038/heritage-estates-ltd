ea6d76fdc4a9fdda83fb75b4c56e47a4db4f955f
karimisharon038 <karimisharon038@gmail.com>
2025-12-19 08:56:04 +0300

    Update chat.js

--- admin-panel/models/chat.js
+++ admin-panel/models/chat.js
@@ -1,8 +1,25 @@
 const mongoose = require('mongoose');
 
-const chatSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  message: { type: String, required: true },
-}, { timestamps: true });
+const chatSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      default: 'Anonymous'
+    },
+    email: {
+      type: String,
+      default: ''
+    },
+    message: {
+      type: String,
+      required: true
+    },
+    read: {
+      type: Boolean,
+      default: false
+    }
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Chat', chatSchema);
